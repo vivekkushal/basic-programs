057ca2e06915ae6300d3de45b36abda5215e81af
vivekkushal <vivekjss1@gmail.com>
2023-01-08 23:26:40 +0530

    Update andOr.py

--- python/andOr.py
+++ python/andOr.py
@@ -1,3 +1,17 @@
-a = input("Enter value of a: ")
+userInput = open("input1.txt", "r")
+# variable = int(userInput.readline()) - will give input as integer line by line
+################### Write code below this #################
 
-print(a)
+a = int(userInput.readline())
+b = int(userInput.readline())
+
+c = a and b
+d = a or b
+
+print("a AND b is:", c)
+print("a OR b is:", d)
+# console.log(`The value of a is: ${a} and the value of b is: ${b}`);
+print(f"The value of a is: {a} and the value of b is: {b}")
+
+################### Write code above this #################
+userInput.close()
