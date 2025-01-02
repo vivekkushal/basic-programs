7698fec1adc2baa448602d6c2fc4b2db325fcb02
vivekkushal <vivekjss1@gmail.com>
2025-01-02 23:28:40 +0530

    feat: Add andOr.cpp

--- cpp/andOr.cpp
+++ cpp/andOr.cpp
@@ -4,14 +4,41 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+void swap(int a, int b) {
+	int temp = a;
+	a = b;
+	b = temp;
+}
+
+void swapPointer(int *a, int *b) {
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+void swapReference(int &a, int &b) {
+	int temp = a;
+	a = b;
+	b = temp;
+}
+
 int main() {
-	int a, b, c, d;
-	cin >> a >> b;
+	// int a, b, c, d;
+	// cin >> a >> b;
 
-	c = a && b;
-	d = a || b;
+	// c = a && b;
+	// d = a || b;
 
-	cout << "a AND b is: " << c << "\n";
-	cout << "a OR b is: " << d << "\n";
-	cout << "The value of a is: " << a << " and the value of b is: " << b << "\n";
+	// cout << "a AND b is: " << c << "\n";
+	// cout << "a OR b is: " << d << "\n";
+	// cout << "The value of a is: " << a << " and the value of b is: " << b << "\n";
+
+	int a = 4, b = 5;
+	cout << "a: " << a << ", b: " << b << "\n";
+	swap(a, b);
+	cout << "a: " << a << ", b: " << b << "\n";
+	// swapPointer(&a, &b);
+	// cout << "a: " << a << ", b: " << b << "\n";
+	swapReference(a, b);
+	cout << "a: " << a << ", b: " << b << "\n";
 }
