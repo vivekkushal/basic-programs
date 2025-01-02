7355fc7f2f5978222bad41fc44385742912f7521
vivekkushal <vivekjss1@gmail.com>
2025-01-02 23:30:22 +0530

    feat: Add calculations.cpp

--- cpp/calculator/calculations.cpp
+++ cpp/calculator/calculations.cpp
@@ -17,11 +17,13 @@ void multiply(double a, double b) {
 }
 
 void divide(double a, double b) {
-	// if (b == 0) {
-	// 	std::cout << "Division by 0 not allowed!" << "\n";
-	// }
+	if (b == 0) {
+		std::cout << "Division by 0 not allowed!" << "\n";
+	} else {
+		// Divide by 0 will give an output of 'inf' so no need to check 'b==0' condition
+		double result = a / b;
+		std::cout << result << "\n";
+		
+	}
 
-	// Divide by 0 will give an output of 'inf' so no need to check 'b==0' condition
-	double result = a / b;
-	std::cout << result << "\n";
 }
