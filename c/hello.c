c89350e8914d16c9702d635a62d7a2a1b9c2b2f4
vivekkushal <vivekjss1@gmail.com>
2025-04-29 09:47:53 +0530

    fix: Indentation

--- c/hello.c
+++ c/hello.c
@@ -2,43 +2,43 @@
 #define ARRAY_SIZE 5
 
 int add3(int a, int b, int c) {
-	return a + b + c;
+    return a + b + c;
 }
 
 int main() {
-	// printf("Hello World!\n");
-	// printf("add3 = %i\n", add3(3, 4, 6));
+    // printf("Hello World!\n");
+    // printf("add3 = %i\n", add3(3, 4, 6));
 
-	// char ch;
-	// ch = getchar();
-	// printf("ch: %c", ch);
+    // char ch;
+    // ch = getchar();
+    // printf("ch: %c", ch);
 
-	// int arr[ARRAY_SIZE];
-	// printf("Enter %d integers of your choice:\n", ARRAY_SIZE);
-	// for (int i = 0; i < ARRAY_SIZE; ++i) {
-	// 	scanf("%d", &arr[i]);
-	// }
-	// printf("Your array is:\n");
-	// printf("[");
-	// for (int i = 0; i < ARRAY_SIZE; ++i) {
-	// 	if (i == 0) printf("%d,", arr[i]);
-	// 	else if (i == ARRAY_SIZE - 1) printf(" %d", arr[i]);
-	// 	else printf(" %d,", arr[i]);
-	// }
-	// printf("]\n");
+    // int arr[ARRAY_SIZE];
+    // printf("Enter %d integers of your choice:\n", ARRAY_SIZE);
+    // for (int i = 0; i < ARRAY_SIZE; ++i) {
+    // 	scanf("%d", &arr[i]);
+    // }
+    // printf("Your array is:\n");
+    // printf("[");
+    // for (int i = 0; i < ARRAY_SIZE; ++i) {
+    // 	if (i == 0) printf("%d,", arr[i]);
+    // 	else if (i == ARRAY_SIZE - 1) printf(" %d", arr[i]);
+    // 	else printf(" %d,", arr[i]);
+    // }
+    // printf("]\n");
 
-	// float arr[ARRAY_SIZE], sum = 0;
-	// int i;
+    // float arr[ARRAY_SIZE], sum = 0;
+    // int i;
 
-	// printf("Enter marks of %d students:\n", ARRAY_SIZE);
-	// for (i = 0; i < ARRAY_SIZE; ++i) {
-	// 	scanf("%f", &arr[i]);
-	// 	sum += arr[i];
-	// }
-	// float avg = sum / ARRAY_SIZE;
-	// printf("The average of the integers is: %f\n", avg);
+    // printf("Enter marks of %d students:\n", ARRAY_SIZE);
+    // for (i = 0; i < ARRAY_SIZE; ++i) {
+    // 	scanf("%f", &arr[i]);
+    // 	sum += arr[i];
+    // }
+    // float avg = sum / ARRAY_SIZE;
+    // printf("The average of the integers is: %f\n", avg);
 
-	char str[] = "Welcome to jenny's lectures";
+    char str[] = "Welcome to jenny's lectures";
     char *ptr = str;
 
     printf("%c\n", *ptr);
